a01108d624e86b237d61e2c0d7e6193ba726503b
nikhiltokas <djntokas@gmail.com>
2024-09-23 00:03:12 +0530

    chapter 05 commit

---
+++ Javascript/Chapter 05 - Conditions/Chapter 05.2 - Exercise.js
@@ -0,0 +1,49 @@
+// Exercise Set
+// Answer are written below, don't cheat. Solve without looking.
+
+// Simple Exercise:
+// Write a program that checks if a number is even or odd using the modulus operator.
+
+
+
+
+// Advanced Exercise:
+// Write a program that accepts a user's age and prints whether the user is a child, teenager, or adult using an if-else if-else structure.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Answer 1
+let number = 4;
+if (number % 2 === 0) {
+  console.log("Even number");
+} else {
+  console.log("Odd number");
+}
+
+
+// Answer 2
+let userAge = 25;
+if (userAge < 13) {
+  console.log("You are a child.");
+} else if (userAge < 18) {
+  console.log("You are a teenager.");
+} else {
+  console.log("You are an adult.");
+}
